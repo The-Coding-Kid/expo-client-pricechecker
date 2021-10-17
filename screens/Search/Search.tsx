d31158a1360483b62936e4ca01df0dea07624cb6
Veerrohit Veeravadivel <veerrohitv08@gmail.com>
2021-10-16 21:52:20 -0700

    able to make the app zoom into our position

--- screens/Search/Search.tsx
+++ screens/Search/Search.tsx
@@ -12,6 +12,8 @@ import {
 } from "@expo-google-fonts/oswald";
 import SearchBar from "../../components/SearchBar";
 var axios = require("axios");
+import { FontAwesome } from "@expo/vector-icons";
+import * as Location from "expo-location";
 
 const Home: React.FC = () => {
   let [fontsLoaded] = useFonts({
@@ -24,21 +26,52 @@ const Home: React.FC = () => {
 
   const [items, setItems] = useState<any>([]);
   const [map, showMap] = useState(false);
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+  const [locationLoaded, setLocationLoaded] = useState<boolean>(false);
+  const [errorMsg, setErrorMsg] = useState<object | string>({});
 
   useEffect(() => {
     axios.get("http://192.168.86.162:5000/items/all").then(async (res: any) => {
       setItems(res.data);
     });
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location1: any = 0;
+      location1 = await Location.getCurrentPositionAsync({
+        accuracy: Location.Accuracy.Lowest,
+      });
+      if (location1 !== 0) {
+        console.log(location1);
+        setLatitude(location1.coords.latitude);
+        setLongitude(location1.coords.longitude);
+      } else {
+        console.log(location1);
+      }
+    })();
   }, []);
 
   useEffect(() => {
-    if (items.length > 0) {
-      showMap(true);
+    if (latitude !== 0 && longitude !== 0) {
+      console.log(latitude, longitude);
+      setLocationLoaded(true);
     }
-  }, [items]);
+  }, [latitude, longitude]);
 
   useEffect(() => {
-    if (map == true) {
+    if (items.length > 0 && latitude !== 0 && longitude !== 0) {
+      showMap(true);
+      setLocationLoaded(true);
+    }
+  }, [items, latitude, longitude]);
+
+  useEffect(() => {
+    if (map == true && locationLoaded == true) {
       const interval = setInterval(() => {
         axios
           .get("http://192.168.86.162:5000/items/all")
@@ -48,7 +81,7 @@ const Home: React.FC = () => {
       }, 100000);
       return () => clearInterval(interval);
     }
-  }, [map]);
+  }, [map, locationLoaded]);
 
   if (!fontsLoaded) {
     return <AppLoading />;
@@ -65,15 +98,17 @@ const Home: React.FC = () => {
             showsUserLocation={true}
             style={styles.map}
             initialRegion={{
-              latitude: 37.78825,
-              longitude: -122.4324,
-              latitudeDelta: 0.0922,
-              longitudeDelta: 0.0421,
+              latitude: latitude,
+              longitude: longitude,
+              latitudeDelta: 0.1,
+              longitudeDelta: 0.05,
             }}
           >
             {items
               ? items.map((item: any) => (
-                  <Marker key={item._id} coordinate={item.coordinate}></Marker>
+                  <Marker key={item._id} coordinate={item.coordinate}>
+                    <FontAwesome name="map-marker" size={24} color="#0096FF" />
+                  </Marker>
                 ))
               : console.log("No items")}
           </MapView>
